d48806e6746a5639fc6662c44a9ef3c7b07a7dc0
esemsc-dh324 <dh324@ic.ac.uk>
2025-11-01 18:28:30 +0000

    feat: pushed the whole project again post rm error

--- src/utils/llm.py
+++ src/utils/llm.py
@@ -4,24 +4,12 @@ from utils.get_env_var import get_env_var
 
 
 class LLMDeployment(StrEnum):
-    GPT_5_CHAT = "gpt-5-chat"
-    GPT_5_MINI = "gpt-5-mini"
     GPT_4_1 = "gpt-4.1"
-    GPT_4_1_MINI = "gpt-4.1-mini"
-    GPT_4_1_NANO = "gpt-4.1-nano"
-    O3_MINI = "o3-mini"
-    MODEL_ROUTER = "model-router"
 
 
 class LLM:
     _deployment_to_env = {
-        LLMDeployment.GPT_5_CHAT: "GPT_5_CHAT_ENDPOINT",
-        LLMDeployment.GPT_5_MINI: "GPT_5_MINI_ENDPOINT",
         LLMDeployment.GPT_4_1: "GPT_41_ENDPOINT",
-        LLMDeployment.GPT_4_1_MINI: "GPT_41_MINI_ENDPOINT",
-        LLMDeployment.GPT_4_1_NANO: "GPT_41_NANO_ENDPOINT",
-        LLMDeployment.O3_MINI: "O3_MINI_ENDPOINT",
-        LLMDeployment.MODEL_ROUTER: "MODEL_ROUTER_ENDPOINT",
     }
 
     def __init__(self, model_deployment: LLMDeployment):
